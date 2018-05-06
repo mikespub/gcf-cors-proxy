e0f4495e9f8e040691f3f36886be541d17bb92e7
mikespub <mikespub@users.noreply.github.com>
2018-05-06 14:29:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Google Cloud Functions - Basic CORS Proxy
+
+This cloud function provides a basic CORS proxy to a predefined HTTPS backend server.
+
+See:
+
+* [Cloud Functions Hello World tutorial][tutorial]
+* [Cloud Functions CORS Proxy sample source code][code]
+
+[tutorial]: https://cloud.google.com/functions/docs/quickstart
+[code]: index.js
+
+## Deploy and run the sample
+
+See the [Cloud Functions Hello World tutorial][tutorial].
+
+## Deploy using the gcloud tool
+
+See [Deploying Cloud Functions][deploying] using the gcloud tool from your [filesystem][filesystem] or [repository][repo]
+
+[deploying]: https://cloud.google.com/functions/docs/deploying/
+[filesystem]: https://cloud.google.com/functions/docs/deploying/filesystem#deploy_using_the_gcloud_tool
+[repo]: https://cloud.google.com/functions/docs/deploying/repo#deploy_using_the_gcloud_tool
---
+++ index.js
@@ -0,0 +1,94 @@
+/**
+ * Google Cloud Functions - Basic CORS Proxy to predefined HTTPS backend server
+ */
+//https://cloud.google.com/functions/docs/bestpractices/networking
+const https = require('https');
+const agent = new https.Agent({keepAlive: true});
+
+const backend = 'pricing.us-east-1.amazonaws.com';
+
+//https://github.com/firebase/functions-samples/blob/master/quickstarts/time-server/functions/index.js
+// CORS Express middleware to enable CORS Requests.
+//const cors = require('cors')({
+//	origin: true,
+//});
+var cachedAgent = function cachedAgent(req, res) {
+	method = req.method || 'GET';
+	path = req.path || '/offers/v1.0/aws/index.json';
+	//If-Modified-Since: Sat, 05 May 2018 20:38:27 GMT
+	modified = req.get('If-Modified-Since');
+	//If-None-Match: "5e5d61fb71903f6b8f0123335154d11b"
+	match = req.get('If-None-Match');
+	headers = {};
+	if (modified) {
+		headers['If-Modified-Since'] = modified;
+	}
+	if (match) {
+		headers['If-None-Match'] = match;
+	}
+	console.log(method + ' ' + path);
+	reqInner = https.request({
+		hostname: backend,
+		port: 443,
+		path: path,
+		method: method,
+		headers: headers,
+		agent: agent
+	}, resInner => {
+		let rawData = '';
+		resInner.setEncoding('utf8');
+		resInner.on('data', chunk => { rawData += chunk; });
+		resInner.on('end', () => {
+			//http://expressjs.com/en/4x/api.html#res
+			res.set('Access-Control-Allow-Origin', "*");
+			res.set('Access-Control-Allow-Methods', "GET, HEAD, OPTIONS");
+			res.set('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+			// CHECKME: what about HEAD or GET 304? We need to expose Content-Length and ETag
+			//https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Expose-Headers
+			res.set('Access-Control-Expose-Headers', 'Content-Length, ETag');
+			//res.status(200).send(`Data: ${rawData}`);
+			//if (method == 'HEAD') {
+			//  console.log(resInner.headers);
+			//}
+			//https://nodejs.org/api/http.html#http_class_http_serverresponse
+			console.log(resInner.statusCode + ' ' + resInner.statusMessage);
+			if (resInner.headers['content-length']) {
+				res.set('Content-Length', resInner.headers['content-length']);
+				console.log('Content-Length: ' + resInner.headers['content-length']);
+			}
+			if (resInner.headers['etag']) {
+				res.set('ETag', resInner.headers['etag']);
+				console.log('ETag:' + resInner.headers['etag']);
+			}
+			//https://cloud.google.com/functions/docs/writing/http
+			if (method == 'HEAD') {
+				res.status(resInner.statusCode).end();
+			} else {
+				res.status(resInner.statusCode).send(rawData);
+			}
+		});
+	});
+	reqInner.on('error', e => {
+		res.status(500).send(`Error: ${e.message}`);
+	});
+	reqInner.end();
+};
+
+/**
+ * HTTP Cloud Function that uses a cached HTTP agent
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.corsAgent = (req, res) => {
+	//https://mhaligowski.github.io/blog/2017/03/10/cors-in-cloud-functions.html
+	//https://github.com/firebase/functions-samples/issues/288
+	//var corsFn = cors();
+	//corsFn(req, res, function() {
+	//	cachedAgent(req, res);
+	//});
+	//cors(req, res, function() {
+	//	cachedAgent(req, res);
+	//});
+	cachedAgent(req, res);
+};
