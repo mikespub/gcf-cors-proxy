38f68307b7f0b06c40ce5501004fe09bb62821a5
mikespub <github@mikespub.net>
2024-07-15 11:32:03 +0000

    update nodejs runtime

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcloud functions deploy corsAgent --trigger-http --runtime nodejs18 --allow-unauthenticated
+gcloud functions deploy corsAgent --trigger-http --runtime nodejs20 --allow-unauthenticated
 # gcloud functions describe corsAgent
